95e7eaaee9d1ba6f4602a90e63557a35e6721a35
Aniruddh Bhandarkar <apb92710@gmail.com>
2021-07-24 20:44:59 +0530

    connect

--- External Python/Project 0.py
+++ External Python/Project 0.py
@@ -1,10 +1,2 @@
-import pandas from py
-n=input("Type the number of tasks that you need to add")
-list1=[High,Medium,Low,Marked as Read]
-list2=[]
-for i in range(1,n+1):
-    list2.append(i)
-py.list1+list2.append()
-print()
 
 
